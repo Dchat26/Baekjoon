efad6ebc1adb4ee8752db02c0736a973850afca3
Dchat <ksmjbh0745@gmail.com>
2026-02-24 18:18:15 +0900

    [Bronze III] Title: 곱셈, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/2588. 곱셈/곱셈.py
@@ -0,0 +1,20 @@
+a = int(input())
+b = input()
+
+b.split()
+
+c = int(b[0])
+d = int(b[1])
+e = int(b[2])
+
+
+f = a * e
+g = a * d
+h = a * c
+
+result = f + (g * 10) + (h * 100)
+
+print(f)
+print(g)
+print(h)
+print(result)
