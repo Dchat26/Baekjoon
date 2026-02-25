b52d8322fb30e9eb336a8dae8b8f6a6d579c9aff
Dchat <ksmjbh0745@gmail.com>
2026-02-25 21:25:44 +0900

    Update 주사위 세개.py

--- 백준/Bronze/2480. 주사위 세개/주사위 세개.py
+++ 백준/Bronze/2480. 주사위 세개/주사위 세개.py
@@ -1,10 +1,27 @@
-a, b, c = map(int, input().split())
+import sys
+input = sys.stdin.readline
 
-if a == b == c:
-    print(10000 + (a * 1000))
-elif a == b or a == c:
-    print(1000 + (a * 100))
-elif b == c:
-    print(1000 + (b * 100))
-else:
-    print(100 * max(a, b, c))
+def dice(a: int, b: int, c: int):
+    if a == b == c:
+        return 10000 + (a * 1000)
+    elif a == b or a == c:
+        return 1000 + (a * 100)
+    elif b == c:
+        return 1000 + (b * 100)
+    else:
+        return 100 * max(a, b, c)
+
+def solve():
+    try:
+        line = input().split()
+        if len(line) < 3: return
+        
+        a, b, c = map(int, line)
+        result = dice(a, b, c)
+        
+        print(result)
+    except ValueError:
+        pass
+
+if __name__ == "__main__":
+    solve()
