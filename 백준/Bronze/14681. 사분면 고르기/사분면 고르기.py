1d722ebaa0db8d33bd0a12dd38ceef72a7466b3c
Dchat <ksmjbh0745@gmail.com>
2026-02-25 18:00:33 +0900

    Update 사분면 고르기.py

--- 백준/Bronze/14681. 사분면 고르기/사분면 고르기.py
+++ 백준/Bronze/14681. 사분면 고르기/사분면 고르기.py
@@ -1,14 +1,23 @@
-x = int(input())
-y = int(input())
+import sys
+input = sys.stdin.readline
 
 def Quadrant(x, y):
     if x > 0 and y > 0:
-        print("1")
+        return 1
     elif x < 0 and y > 0:
-        print("2")
+        return 2
     elif x < 0 and y < 0:
-        print("3")
-    else:
-        print("4")
-        
-Quadrant(x, y)
+        return 3
+    elif x > 0 and y < 0:
+        return 4
+
+def solve():
+    x = int(input().rstrip())
+    y = int(input().rstrip())
+    
+    result = Quadrant(x, y)
+    
+    print(result)
+
+if __name__ == "__main__":
+    solve()
