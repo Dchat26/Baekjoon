087391e4e19f759e65aa3e9fbea04cad7583116b
Dchat <ksmjbh0745@gmail.com>
2026-02-26 18:36:34 +0900

    Update 합.py

--- 백준/Bronze/8393. 합/합.py
+++ 백준/Bronze/8393. 합/합.py
@@ -1,3 +1,22 @@
-n = int(input())
+import sys
+input = sys.stdin.readline
 
-print(sum(range(1, n + 1)))
+def Total(n: int):
+    total = 0
+    for i in range(1, n + 1):
+        total += i
+        
+    print(total)
+
+def solve():
+    try:
+        line = input().rstrip()
+        if not line: return
+        n = int(line)
+        
+        Total(n)
+    except ValueError:
+        pass
+
+if __name__ == "__main__":
+    solve()
