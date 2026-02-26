7ef3b03c1b93483905a7d0411fa86a3154648712
Dchat <ksmjbh0745@gmail.com>
2026-02-26 20:28:12 +0900

    [Bronze IV] Title: 별 찍기 - 2, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2439. 별 찍기 － 2/별 찍기 － 2.cc
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+void Star(int n) {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    for (int i = 1; i <= n; i++) {
+        for (int j = 1; j <= n - i; j++) {
+            cout << " ";
+        }
+        
+        for (int k = 1; k <= i; k++) {
+            cout << "*";
+        }
+        cout << "\n";
+    }
+}
+
+int main(void) {
+    int n = 0;
+
+    if (!(cin >> n)) return -1;
+
+    Star(n);
+
+    return 0;
+}
