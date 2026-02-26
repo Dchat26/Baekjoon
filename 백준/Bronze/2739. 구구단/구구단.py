f1a099bee912246d5010dea0dec93ee5de82336d
Dchat <ksmjbh0745@gmail.com>
2026-02-26 18:22:52 +0900

    Update 구구단.py

--- 백준/Bronze/2739. 구구단/구구단.py
+++ 백준/Bronze/2739. 구구단/구구단.py
@@ -1,4 +1,19 @@
-n = int(input())
+import sys
+input = sys.stdin.readline
 
-for i in range(1, 10):
-    print("{0} * {1} = {2}" .format(n, i, n * i))
+def GuGu(num: int):
+    for i in range(1, 10):
+        print(f"{num} * {i} = {num * i}")
+
+def solve():
+    try:
+        line = input().rstrip()
+        if not line: return
+        
+        num = int(line)
+        GuGu(num)
+    except ValueError:
+        pass
+
+if __name__ == "__main__":
+    solve()
