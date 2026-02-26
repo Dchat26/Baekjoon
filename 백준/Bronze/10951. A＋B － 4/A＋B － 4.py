3d6820657f40779d260aeadedfbae08fa887e36b
Dchat <ksmjbh0745@gmail.com>
2026-02-26 20:47:57 +0900

    Update A＋B － 4.py

--- 백준/Bronze/10951. A＋B － 4/A＋B － 4.py
+++ 백준/Bronze/10951. A＋B － 4/A＋B － 4.py
@@ -1,6 +1,23 @@
 import sys
 
-input = sys.stdin.readline
+def solve():
+    stdin_read = sys.stdin.read
+    stdout_write = sys.stdout.write
 
-for line in sys.stdin:
-    print(sum(map(int, line.split())))
+    try:
+        input_data = stdin_read().split()
+        if not input_data:
+            return
+        
+        results = []
+        for i in range(0, len(input_data), 2):
+            a = int(input_data[i])
+            b = int(input_data[i+1])
+            results.append(str(a + b))
+        
+        stdout_write("\n".join(results) + "\n")
+    except EOFError:
+        pass
+
+if __name__ == "__main__":
+    solve()
