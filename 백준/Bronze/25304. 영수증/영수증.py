5e480e746790d585aabaf74c1418285ce934c2bc
Dchat <ksmjbh0745@gmail.com>
2026-02-26 18:44:55 +0900

    Update 영수증.py

--- 백준/Bronze/25304. 영수증/영수증.py
+++ 백준/Bronze/25304. 영수증/영수증.py
@@ -1,12 +1,33 @@
-full_price = int(input())
-numbers = int(input())
-price_number = [list(map(int, input().split())) for i in range(numbers)]
+import sys
+input = sys.stdin.readline
 
-result = 0
-for i in range(numbers):
-    result += (price_number[i][0] * price_number[i][1])
-    
-if result == full_price:
-    print("Yes")
-else:
-    print("No")
+def ynPrice(total: int, n: int):
+    CalPrice = 0
+    for _ in range(n):
+        line = input().split()
+        if not line: return
+        
+        a, b = map(int, line)
+        CalPrice += a * b
+        
+    if total == CalPrice:
+        print("Yes")
+    else:
+        print("No")
+
+def solve():
+    try:
+        line = input().rstrip()
+        if not line: return
+        total_price = int(line)
+        
+        line = input().rstrip()
+        if not line: return
+        n = int(line)
+        
+        ynPrice(total_price, n)
+    except ValueError:
+        pass
+
+if __name__ == "__main__":
+    solve()
