a437c1daded8ac3028d746747de6e07ca8a30e3a
Dchat <ksmjbh0745@gmail.com>
2026-02-26 19:30:03 +0900

    Update A＋B － 8.py

--- 백준/Bronze/11022. A＋B － 8/A＋B － 8.py
+++ 백준/Bronze/11022. A＋B － 8/A＋B － 8.py
@@ -1,12 +1,25 @@
 import sys
-
 input = sys.stdin.readline
 
-n = int(input())
+def Mul(n: int):
+    for i in range(1, n + 1):
+        line = input().split()
+        if not line: return
+        
+        a, b = map(int, line)
+        
+        print(f"Case #{i}: {a} + {b} = {a + b}")
 
-n_list = [list(map(int, input().split())) for i in range(n)]
+def solve():
+    try:
+        line = input().rstrip()
+        if not line: return
+        n = int(line)
+        
+        Mul(n)
+        
+    except ValueError:
+        pass
 
-for i in range(n):
-    a, b = n_list[i]
-    
-    print(f"Case #{i + 1}: {a} + {b} = {a + b}")
+if __name__ == "__main__":
+    solve()
