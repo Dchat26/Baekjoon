b106e8385fe8983f008bfc5c6d1f41b88139cfbf
Dchat <ksmjbh0745@gmail.com>
2026-03-01 02:18:48 +0900

    Update 바구니 뒤집기.py

--- 백준/Bronze/10811. 바구니 뒤집기/바구니 뒤집기.py
+++ 백준/Bronze/10811. 바구니 뒤집기/바구니 뒤집기.py
@@ -1,9 +1,28 @@
-N, M = map(int, input().split())
+import sys
+input = sys.stdin.read
 
-n_list = list(range(1, N + 1))
-
-for i in range(M):
-    a, b = map(int, input().split())
-    n_list[a-1:b] = n_list[a-1:b][::-1]
+def swap_basket(data: list[str]):
+    n = int(data[0])
+    m = int(data[1])
     
-print(*n_list)
+    nlist = list(range(1, n + 1))
+    
+    current = 2
+    for _ in range(m):
+        i = int(data[current]) - 1
+        j = int(data[current + 1])
+        
+        nlist[i:j] = nlist[i:j][::-1]
+        
+        current += 2
+    
+    print(*nlist)
+        
+def solve():
+    data = input().split()
+    if not data: return
+
+    swap_basket(data)
+
+if __name__ == "__main__":
+    solve()
