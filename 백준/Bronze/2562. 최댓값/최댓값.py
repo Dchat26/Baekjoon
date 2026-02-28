a2d9f7e7c3b30a8206d2af868982362541cf6bc7
Dchat <ksmjbh0745@gmail.com>
2026-02-28 23:55:39 +0900

    Update 최댓값.py

--- 백준/Bronze/2562. 최댓값/최댓값.py
+++ 백준/Bronze/2562. 최댓값/최댓값.py
@@ -4,9 +4,11 @@ input = sys.stdin.readline
 def MaxIndex(nlist: list[int]):
     max_val = max(nlist)
     print(max_val, (nlist.index(max_val) + 1), sep="\n")
+    
 def solve():
     nlist = [int(input()) for _ in range(9)]
     
     MaxIndex(nlist)
 if __name__ == "__main__":
     solve()
+
