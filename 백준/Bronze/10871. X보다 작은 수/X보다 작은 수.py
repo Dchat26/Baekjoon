a4d07b8d52bd38ffa142dc88003fde122a953daf
Dchat <ksmjbh0745@gmail.com>
2026-02-28 23:23:00 +0900

    Update X보다 작은 수.py

--- 백준/Bronze/10871. X보다 작은 수/X보다 작은 수.py
+++ 백준/Bronze/10871. X보다 작은 수/X보다 작은 수.py
@@ -1,4 +1,22 @@
-n, f = map(int, input().split())
-n_list = list(map(int, input().split()))
+import sys
+input = sys.stdin.readline
 
-print(*(i for i in n_list if i < f))
+def LessPrint(nlist: list[int], target: int):
+    for i in nlist:
+        if i < target:
+            print(i, end=' ')
+
+def solve():
+    try:
+        NX = input().split()
+        if not NX: return
+        N, X = map(int, NX)
+        
+        N_List = list(map(int, input().split()))
+        
+        LessPrint(N_List, X)
+    except (ValueError, EOFError):
+        pass
+
+if __name__ == "__main__":
+    solve()
