62198e84b492700fcea453e4c840990b352f5150
Dchat <ksmjbh0745@gmail.com>
2026-03-04 00:26:58 +0900

    Update 너의 평점은.py

--- 백준/Silver/25206. 너의 평점은/너의 평점은.py
+++ 백준/Silver/25206. 너의 평점은/너의 평점은.py
@@ -1,14 +1,23 @@
 import sys
+input = sys.stdin.read
 
-score_board = {'A+': 4.5, 'A0': 4.0, 'B+': 3.5, 'B0': 3.0, 'C+': 2.5, 'C0': 2.0, 'D+': 1.5, 'D0': 1.0, 'F': 0.0}
-
-score_sum = 0
-score_class = 0
-for line in sys.stdin:
-    s = line.split()
-    if s[2] == 'P':
-        continue
-    score_class += float(s[1]) * score_board[s[2]]
-    score_sum += float(s[1])
+def solve():
+    data = input().split()
+    if not data: return
     
-print(score_class / score_sum)
+    score_board = {'A+' : 4.5, 'A0' : 4.0, 'B+' : 3.5, 'B0' : 3.0,
+                   'C+' : 2.5, 'C0' : 2.0, 'D+' : 1.5, 'D0' : 1.0, 'F' : 0.0}
+    
+    score_sum = 0
+    score_class = 0
+    for i in range(0, len(data), 3):
+        if data[i+2] == 'P':
+            continue
+        
+        score_sum += float(data[i+1])
+        score_class += float(data[i+1]) * score_board[data[i+2]]
+    
+    print(score_class / score_sum)
+
+if __name__ == "__main__":
+    solve()
