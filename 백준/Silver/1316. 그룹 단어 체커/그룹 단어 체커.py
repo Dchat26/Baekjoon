1f6d1b39c9188f5b393bb489161ca6c3f34cc337
Dchat <ksmjbh0745@gmail.com>
2026-03-03 23:55:32 +0900

    Update 그룹 단어 체커.py

--- 백준/Silver/1316. 그룹 단어 체커/그룹 단어 체커.py
+++ 백준/Silver/1316. 그룹 단어 체커/그룹 단어 체커.py
@@ -1,17 +1,24 @@
-n = int(input())
+import sys
+input = sys.stdin.readline
 
-count = 0
-for _ in range(n):
-    s = input()
-    is_count = True
+def solve():
+    n = int(input().rstrip())
+    if not n: return
     
-    for i in range(len(s) - 1):
-        if s[i] != s[i+1]:
-            if s[i] in s[i+1:]:
-                is_count = False
-                break
-            
-    if is_count:
-        count += 1
-
-print(count)
+    count = 0
+    for _ in range(n):
+        s = input().rstrip()
+        if not s: return
+        
+        is_count = True
+        for i in range(len(s) - 1):
+            if s[i] != s[i+1]:
+                if s[i] in s[i+1:]:
+                    is_count = False
+        
+        if is_count: count += 1
+    
+    print(count)
+    
+if __name__ == "__main__":
+    solve()
