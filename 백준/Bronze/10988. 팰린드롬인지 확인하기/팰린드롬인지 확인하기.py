1459a95dce39bd7bf9feb23b7a9cfad1a043550b
Dchat <ksmjbh0745@gmail.com>
2026-03-03 22:51:03 +0900

    Update 팰린드롬인지 확인하기.py

--- 백준/Bronze/10988. 팰린드롬인지 확인하기/팰린드롬인지 확인하기.py
+++ 백준/Bronze/10988. 팰린드롬인지 확인하기/팰린드롬인지 확인하기.py
@@ -1,3 +1,12 @@
-s = input()
-  
-print('1' if s == ''.join(reversed(s)) else '0')
+import sys
+input = sys.stdin.readline
+    
+def solve():
+    s = input().rstrip()
+    if not s: return
+    
+    if s == "".join(reversed(s)): print(1)
+    else: print(0)
+    
+if __name__ == "__main__":
+    solve()
