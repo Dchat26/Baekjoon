2b69b0229d8b807b3a51f99e536f86ef64a897de
Dchat <ksmjbh0745@gmail.com>
2026-03-03 22:22:57 +0900

    Update 킹， 퀸， 룩， 비숍， 나이트， 폰.py

--- 백준/Bronze/3003. 킹， 퀸， 룩， 비숍， 나이트， 폰/킹， 퀸， 룩， 비숍， 나이트， 폰.py
+++ 백준/Bronze/3003. 킹， 퀸， 룩， 비숍， 나이트， 폰/킹， 퀸， 룩， 비숍， 나이트， 폰.py
@@ -1,10 +1,21 @@
-n = list(map(int, input().split()))
+import sys
+input = sys.stdin.readline
 
-n_list = [1, 1, 2, 2, 2, 8]
-
-r_list = []
-for i in range(6):
-    r = n_list[i] - n[i]
-    r_list.append(r)
+def check_p(plist: list[int], piece: list[int]):
+    result = []
+    for i in range(6):
+        result.append(piece[i] - plist[i])
+        
+    print(*result)
+        
+def solve():
+    line = input().split()
+    if not line: return
     
-print(*r_list)
+    plist = list(map(int, line))
+    piece = [1, 1, 2, 2, 2, 8]
+    
+    check_p(plist, piece)
+    
+if __name__ == "__main__":
+    solve()
