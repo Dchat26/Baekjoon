c779cd7b375e41b5922febf98d0207733bee4a95
Dchat <ksmjbh0745@gmail.com>
2026-03-03 22:04:51 +0900

    [Bronze V] Title: 새싹, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/25083. 새싹/새싹.cc
+++ 백준/Bronze/25083. 새싹/새싹.cc
@@ -1,17 +1,16 @@
-#include<iostream>
-
+#include <iostream>
 using namespace std;
 
-int main() {
-    cin.tie(nullptr);
-    ios_base::sync_with_stdio(false);
-    
+int main(void) {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
     cout << "         ,r\'\"7" << '\n';
     cout << "r`-_   ,\'  ,/" << '\n';
     cout << " \\. \". L_r\'" << '\n';
     cout << "   `~\\/" << '\n';
     cout << "      |" << '\n';
     cout << "      |" << '\n';
-    
+
     return 0;
 }
