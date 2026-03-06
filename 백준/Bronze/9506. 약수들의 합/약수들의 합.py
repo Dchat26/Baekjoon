6bafc9cdcfe138891f0435aa80101cdd8d0b3cf8
Dchat <ksmjbh0745@gmail.com>
2026-03-06 18:47:09 +0900

    Update 약수들의 합.py

--- 백준/Bronze/9506. 약수들의 합/약수들의 합.py
+++ 백준/Bronze/9506. 약수들의 합/약수들의 합.py
@@ -1,13 +1,23 @@
 import sys
 
-for line in sys.stdin:
-    line = int(line)
-    if line == -1:
-        break
-    
-    line_list = [i for i in range(1, line) if line % i == 0]
-    if sum(line_list) == line:
-        print(f'{line} = ', end='')
-        print(" + ".join(map(str, line_list)))
-    else:
-        print(f'{line} is NOT perfect.')
+def solve():
+    for line in sys.stdin:
+        if not line: return
+        n = int(line.rstrip())
+        if n == -1: return
+        
+        nsum = 0
+        nlist = []
+        for i in range(1, n):
+            if n % i == 0:
+                nsum += i
+                nlist.append(i)
+        
+        if n == nsum:
+            print(n, "= ", end="")
+            print(" + ".join(map(str, nlist)))
+        else:
+            print(n, "is NOT perfect.")
+            
+if __name__ == "__main__":
+    solve()
