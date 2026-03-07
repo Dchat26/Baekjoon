dfd10d606eee3ff819aef135f0c6d12270d7398b
Dchat <ksmjbh0745@gmail.com>
2026-03-07 18:24:29 +0900

    [Bronze III] Title: 네 번째 점, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/3009. 네 번째 점/네 번째 점.py
+++ 백준/Bronze/3009. 네 번째 점/네 번째 점.py
@@ -2,25 +2,17 @@ import sys
 input = sys.stdin.readline
 
 def solve():
-    xlist = []
-    ylist = []
-    for _ in range(3):
-        line = input().split()
-        if not line: return
-        
-        x, y = map(int, line)
-        xlist.append(x)
-        ylist.append(y)
-    
     x4 = 0
     y4 = 0
-    for i in range(3):
-        if xlist.count(xlist[i]) == 1:
-            x4 = xlist[i]
-        if ylist.count(ylist[i]) == 1:
-            y4 = ylist[i]
-            
+    for _ in range(3):
+        line = input().split()
+        if not line: return 
+        
+        x, y = map(int, line)
+        x4 ^= x
+        y4 ^= y
+        
     print(x4, y4)
-            
+    
 if __name__ == "__main__":
     solve()
