a210839e8bba04719615c4f2a51868d02c34f0e3
Dchat <ksmjbh0745@gmail.com>
2026-03-08 18:08:41 +0900

    [Bronze IV] Title: 알고리즘 수업 - 알고리즘의 수행 시간 2, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/24263. 알고리즘 수업 － 알고리즘의 수행 시간 2/알고리즘 수업 － 알고리즘의 수행 시간 2.cc
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main(void) {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int n = 0;
+    if (!(cin >> n)) return 0;
+
+    cout << n << "\n" << 1 << "\n";
+
+    return 0;
+}
