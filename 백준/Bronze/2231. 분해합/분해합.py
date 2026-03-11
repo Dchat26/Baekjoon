eb7c04d9044552f346f05a5e2e5cd9f4c6b50004
Dchat <ksmjbh0745@gmail.com>
2026-03-11 16:46:45 +0900

    [Bronze II] Title: 분해합, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/2231. 분해합/분해합.py
+++ 백준/Bronze/2231. 분해합/분해합.py
@@ -1,9 +1,22 @@
-n = int(input())
+import sys
+input = sys.stdin.readline
 
-for i in range(1, n + 1):
-    sumn = sum(map(int, str(i)))
-    if sumn + i == n:
-        print(i)
-        break
-else:
+def solve():
+    n = input().rstrip()
+    if not n: return
+    
+    nlen = len(n)
+    n = int(n)
+    start = max(0, n - 9 * nlen)
+
+    for i in range(start, n + 1):
+        sumn = sum(map(int, str(i)))
+        
+        if i + sumn == n:
+            print(i)
+            return
+        
     print(0)
+    
+if __name__ == "__main__":
+    solve()
