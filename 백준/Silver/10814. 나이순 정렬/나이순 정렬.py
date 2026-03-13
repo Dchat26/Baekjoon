24b15191187cf08d56fbbc6e1e1a2f27e211e4a6
Dchat <ksmjbh0745@gmail.com>
2026-03-13 20:44:54 +0900

    [Silver V] Title: 나이순 정렬, Time: 136 ms, Memory: 51620 KB -BaekjoonHub

--- 백준/Silver/10814. 나이순 정렬/나이순 정렬.py
+++ 백준/Silver/10814. 나이순 정렬/나이순 정렬.py
@@ -1,8 +1,28 @@
-n = int(input())
-
-n_list = [(int(a), b) for a, b in (input().split() for _ in range(n))]
-
-n_list.sort(key=lambda x: x[0])
-
-for i in n_list:
-    print(*i)
+import sys
+input = sys.stdin.readline
+write = sys.stdout.write
+        
+def solve():
+    n = int(input().rstrip())
+    if not n: return
+    if not 1 <= n or not n <= 100000: return
+    
+    nlist = []
+    for _ in range(n):
+        line = input().split()
+        if not line: return
+        
+        a, b = line
+        a = int(a)
+        if not 1 <= a or not a <= 200: return
+        
+        nlist.append((a, b))
+        
+    nlist.sort(key=lambda x: x[0])
+    
+    result = [f"{age} {name}" for age, name in nlist]
+    
+    write("\n".join(result) + "\n")
+    
+if __name__ == "__main__":
+    solve()
