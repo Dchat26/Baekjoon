900476586703f78937694209a33288b49a9b1058
Dchat <ksmjbh0745@gmail.com>
2026-03-14 12:32:35 +0900

    [Silver V] Title: 숫자 카드, Time: 580 ms, Memory: 118016 KB -BaekjoonHub

--- 백준/Silver/10815. 숫자 카드/숫자 카드.py
+++ 백준/Silver/10815. 숫자 카드/숫자 카드.py
@@ -1,33 +1,13 @@
 import sys
-input = sys.stdin.readline
-write = sys.stdout.write
 
-def solve():
-    n = int(input().rstrip())
-    if not n: return
-    nlist = sorted(list(map(int, input().split())))
-    
-    m = int(input().rstrip())
-    if not m: return
-    mlist = list(map(int, input().split()))
-    
-    result = []
-    for i in mlist:
-        start, end = 0, n - 1
-        found = False
-        while start <= end:
-            mid = (start + end) // 2
-            if nlist[mid] == i:
-                found = True
-                break
-            elif nlist[mid] < i:
-                start = mid + 1
-            else:
-                end = mid - 1
-                
-        result.append("1" if found else "0")
-        
-    write(" ".join(result) + "\n")
-    
-if __name__ == "__main__":
-    solve()
+n = sys.stdin.readline()
+n_list = set(sys.stdin.readline().split())
+
+m = sys.stdin.readline()
+m_list = sys.stdin.readline().split()
+
+for i in m_list:
+    if i in n_list:
+        print(1, end=' ')
+    else:
+        print(0, end=' ')
