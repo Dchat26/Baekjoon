cbb69a37d904f93d06735188ee975b0fcd0e37d2
Dchat <ksmjbh0745@gmail.com>
2026-03-17 22:48:44 +0900

    [Silver IV] Title: 큐 2, Time: 420 ms, Memory: 28460 KB -BaekjoonHub

--- 백준/Silver/18258. 큐 2/큐 2.cc
+++ 백준/Silver/18258. 큐 2/큐 2.cc
@@ -1,7 +1,20 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 
-#define MAX_SIZE 2000000
+#define MAX_SIZE 2000005
+#define BUFFER_SIZE 20000000
+
+char output_buffer[BUFFER_SIZE];
+char* ptr = output_buffer;
+
+void add_to_buffer(int value) {
+	ptr += sprintf(ptr, "%d\n", value);
+}
+
+void add_str_to_buffer(const char* str) {
+	ptr += sprintf(ptr, "%s\n", str);
+}
 
 typedef struct {
 	int data[MAX_SIZE];
@@ -10,37 +23,38 @@ typedef struct {
 } Stack;
 
 void init(Stack* s) {
-	s->top = -1;
+	s->top = 0;
 	s->bottom = 0;
 }
 
 void push(Stack* s, int x) {
-	if (s->top >= MAX_SIZE - 1) return;
-	s->data[++(s->top)] = x;
+	if ((s->top + 1) % MAX_SIZE == s->bottom) return;
+	s->top = (s->top + 1) % MAX_SIZE;
+	s->data[(s->top)] = x;
 }
 
 int pop(Stack* s) {
-	if (s->bottom > s->top) return -1;
-	return s->data[(s->bottom)++];
+	if (s->bottom == s->top) return -1;
+	s->bottom = (s->bottom + 1) % MAX_SIZE;
+	return s->data[(s->bottom)];
 }
 
 int size(Stack* s) {
-	if (s->bottom > s->top) return 0;
-	return s->top - s->bottom + 1;
+	return (s->top - s->bottom + MAX_SIZE) % MAX_SIZE;
 }
 
 int empty(Stack* s) {
-	if (s->bottom > s->top) return 1;
+	if (s->bottom == s->top) return 1;
 	return 0;
 }
 
 int front(Stack* s) {
-	if (s->bottom > s->top) return -1;
-	return s->data[(s->bottom)];
+	if (s->bottom == s->top) return -1;
+	return s->data[(s->bottom + 1) % MAX_SIZE];
 }
 
 int back(Stack* s) {
-	if (s->bottom > s->top) return -1;
+	if (s->bottom == s->top) return -1;
 	return s->data[(s->top)];
 }
 
@@ -62,21 +76,23 @@ int main(void) {
 			push(&myStack, x);
 		}
 		else if (strcmp(s, "pop") == 0) {
-			printf("%d\n", pop(&myStack));
+			add_to_buffer(pop(&myStack));
 		}
 		else if (strcmp(s, "size") == 0) {
-			printf("%d\n", size(&myStack));
+			add_to_buffer(size(&myStack));
 		}
 		else if (strcmp(s, "empty") == 0) {
-			printf("%d\n", empty(&myStack));
+			add_to_buffer(empty(&myStack));
 		}
 		else if (strcmp(s, "front") == 0) {
-			printf("%d\n", front(&myStack));
+			add_to_buffer(front(&myStack));
 		}
 		else if (strcmp(s, "back") == 0) {
-			printf("%d\n", back(&myStack));
+			add_to_buffer(back(&myStack));
 		}
 	}
+
+	fwrite(output_buffer, 1, ptr - output_buffer, stdout);
 
 	return 0;
 }
