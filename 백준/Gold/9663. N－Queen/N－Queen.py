6a16544a17d5c8c982587db6f2916a68f7bf272a
Dchat <ksmjbh0745@gmail.com>
2026-03-23 01:54:43 +0900

    [Gold IV] Title: N-Queen, Time: 18796 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Gold/9663. N－Queen/N－Queen.py
+++ 백준/Gold/9663. N－Queen/N－Queen.py
@@ -1,33 +1,35 @@
 import sys
-input = sys.stdin.readline
 
-def solve(row, n, col, diag1, diag2):
+input = sys.stdin.readline
+write = sys.stdout.write
+
+def Queen(row: int, n: int, col: list[bool], diag1: list[bool], diag2: list[bool], count_list: list[int]):
     global count
     
     if row == n:
-        count += 1
+        count_list[0] += 1
         return
     
     for i in range(n):
         if not col[i] and not diag1[row + i] and not diag2[row - i + n]:
             col[i] = diag1[row + i] = diag2[row - i + n] = True
             
-            solve(row + 1, n, col, diag1, diag2)
+            Queen(row + 1, n, col, diag1, diag2, count_list)
             
             col[i] = diag1[row + i] = diag2[row - i + n] = False
             
-def main():
-    n = int(input())
-    global count
-    count = 0
+def solve():
+    n = int(input().rstrip())
+    if not n: return
     
+    count_list = [0]
     col = [False] * n
     diag1 = [False] * (2 * n)
     diag2 = [False] * (2 * n)
     
-    solve(0, n, col, diag1, diag2)
+    Queen(0, n, col, diag1, diag2, count_list)
     
-    print(count)
+    write(str(count_list[0]) + "\n")
     
 if __name__ == "__main__":
-    main()
+    solve()
