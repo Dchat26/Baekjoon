f575c0820ef59a241cd148473ee3559b51998a7a
Dchat <ksmjbh0745@gmail.com>
2026-03-23 00:57:56 +0900

    [Silver III] Title: N과 M (2), Time: 40 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.py
@@ -0,0 +1,30 @@
+import sys
+
+input = sys.stdin.readline
+write = sys.stdout.write
+
+def sq(depth: int, start: int, m: int, result: list[str], ans: list[int], n: int):
+    if depth == m:
+        result.append(" ".join(map(str, ans)))
+    
+    for i in range(start, n + 1):
+            ans.append(i)
+            
+            sq(depth + 1, i + 1, m, result, ans, n)
+            
+            ans.pop()
+            
+def solve():
+    line = input().split()
+    if not line: return
+    n, m = map(int, line)
+    
+    result = []
+    ans = []
+    
+    sq(0, 1, m, result, ans, n)
+    
+    write("\n".join(result) + "\n")
+
+if __name__ == "__main__":
+    solve()
